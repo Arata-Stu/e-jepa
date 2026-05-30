98ee2421a10be55d593cd6c1f858d1b5b87a8cd1
Tanaka Arata <ky.37f.9850@s.thers.ac.jp>
2026-05-30 22:14:28 +0900

    feat: add DOWNSTREAM_NUM_WORKERS variable for configurable worker count in ablation scripts

--- scripts/experiments/downstream_m3ed_semantic_t1_240x320_ablation.sh
+++ scripts/experiments/downstream_m3ed_semantic_t1_240x320_ablation.sh
@@ -22,6 +22,7 @@ MAE_CKPT="${MAE_CKPT:-}"
 
 DOWNSTREAM_EPOCHS="${DOWNSTREAM_EPOCHS:-100}"
 DOWNSTREAM_CLIP_NUM_FRAMES="${DOWNSTREAM_CLIP_NUM_FRAMES:-8}"
+DOWNSTREAM_NUM_WORKERS="${DOWNSTREAM_NUM_WORKERS:-16}"
 
 RUN_JEPA_LINEAR="${RUN_JEPA_LINEAR:-1}"
 RUN_JEPA_FINETUNE="${RUN_JEPA_FINETUNE:-1}"
@@ -81,6 +82,7 @@ COMMON_TASK_ARGS=(
   "task.eval_original_resolution=true"
   "task.eval_logits_resize_mode=nearest"
   "task.clip_num_frames=${DOWNSTREAM_CLIP_NUM_FRAMES}"
+  "task.num_workers=${DOWNSTREAM_NUM_WORKERS}"
   "model.num_frames=${DOWNSTREAM_CLIP_NUM_FRAMES}"
   "optimization.epochs=${DOWNSTREAM_EPOCHS}"
 )
